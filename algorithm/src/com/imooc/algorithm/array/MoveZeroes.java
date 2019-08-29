da460ab74def26f60c10c1fd35816affc3aa32c7
louis <244187563@qq.com>
2019-08-29 20:41:24 +0800

    cursor k

--- algorithm/src/com/imooc/algorithm/array/MoveZeroes.java
+++ algorithm/src/com/imooc/algorithm/array/MoveZeroes.java
@@ -1,5 +1,8 @@
 package com.imooc.algorithm.array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MoveZeroes {
 
     /**
@@ -9,17 +12,65 @@ public class MoveZeroes {
      * 空间复杂度：
      */
     public void moveZeroes(int[] nums){
-//        int index = 0;
-//        for(int i=0; i<nums.length; i++){
+//        // swap 0和非0
+//        // 时间复杂度：O(n^2)
+//        // 空间复杂度：O(1)
+//        for (int i=0; i<nums.length; i++){
 //            if(nums[i]==0){
-//                nums[i]=nums[i++];
-//                nums[i++]=0;
-//                index++;
+//                for(int j=i+1; j<nums.length; j++){
+//                    if(nums[j]!=0){
+//                        int t = nums[j];
+//                        nums[j] = nums[i];
+//                        nums[i] = t;
+//                        break;
+//                    }
+//                }
 //            }
-//
 //        }
-//        for (int i=0; i<index; i++)
-//            nums[nums.length-1-i]=0;
+//
+//        // 直接取出非0元素放在前面
+//        // 时间复杂度：O(n)
+//        // 空间复杂度：O(n)
+//        List<Integer> nonZeroList = new ArrayList<Integer>();
+//        for(int i=0; i<nums.length; i++){
+//            if(nums[i]!=0)
+//                nonZeroList.add(nums[i]);
+//        }
+//
+//        for(int i=0; i<nonZeroList.size(); i++){
+//            nums[i] = nonZeroList.get(i);
+//        }
+//
+//        for(int i=nonZeroList.size(); i<nums.length; i++){
+//            nums[i] = 0;
+//        }
+//
+//        // 优化1：原地，不用O(n)的空间
+//        // 时间复杂度：O(n)
+//        // 空间复杂度：O(1)
+//        int k = 0;
+//        for(int i=0; i<nums.length; i++)
+//            if(nums[i]!=0)
+//                nums[k++] = nums[i];
+//        for(int i=k; i<nums.length; i++)
+//            nums[i] = 0;
+
+        // 优化2：swap 0和非0（用游标k，就不用再for循环了）
+        // 时间复杂度：O(n)
+        // 空间复杂度：O(1)
+        int k = 0;
+        for(int i=0; i<nums.length; i++)
+            if(nums[i]!=0){
+                // swap(nums[i], nums[k])
+                if(i!=k){
+                    int t = nums[i];
+                    nums[i] = nums[k];
+                    nums[k] = t;
+                    k++;
+                }
+                else // i==k
+                    k++;
+            }
     }
 
     // main
@@ -27,7 +78,7 @@ public class MoveZeroes {
         int[] arr = {0,1,0,3,12};
         (new MoveZeroes()).moveZeroes(arr);
         for(int i=0; i<arr.length; i++)
-            System.out.print(i + ", ");
+            System.out.print(arr[i] + " ");
         System.out.println();
 
     }
