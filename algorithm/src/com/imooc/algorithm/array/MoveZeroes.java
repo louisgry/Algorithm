d78c26507509336e51424cf5db86dc9d60f528e4
louis <244187563@qq.com>
2019-09-03 14:26:32 +0800

    quick commit

--- algorithm/src/com/imooc/algorithm/array/MoveZeroes.java
+++ algorithm/src/com/imooc/algorithm/array/MoveZeroes.java
@@ -8,13 +8,15 @@ public class MoveZeroes {
     /**
      * 283. Move Zeroes
      * https://leetcode.com/problems/move-zeroes/
-     * 时间复杂度：
-     * 空间复杂度：
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
      */
     public void moveZeroes(int[] nums){
-//        // swap 0和非0
-//        // 时间复杂度：O(n^2)
-//        // 空间复杂度：O(1)
+        /**
+         * 思路1：两个for循环，第一个for找0，第二个找非0，然后swap
+         * 时间复杂度：O(n^2)
+         * 空间复杂度：O(1)
+         */
 //        for (int i=0; i<nums.length; i++){
 //            if(nums[i]==0){
 //                for(int j=i+1; j<nums.length; j++){
@@ -28,9 +30,11 @@ public class MoveZeroes {
 //            }
 //        }
 //
-//        // 直接取出非0元素放在前面
-//        // 时间复杂度：O(n)
-//        // 空间复杂度：O(n)
+        /**
+         * 思路2：用一个List存非零元素，然后把nums的最后几位置为0
+         * 时间复杂度：O(n)
+         * 空间复杂度：O(n)
+         */
 //        List<Integer> nonZeroList = new ArrayList<Integer>();
 //        for(int i=0; i<nums.length; i++){
 //            if(nums[i]!=0)
@@ -45,9 +49,11 @@ public class MoveZeroes {
 //            nums[i] = 0;
 //        }
 //
-//        // 优化1：原地，不用O(n)的空间
-//        // 时间复杂度：O(n)
-//        // 空间复杂度：O(1)
+        /**
+         * 思路3：采用原地算法，不使用List，用游标k替代
+         * 时间复杂度：O(n)
+         * 空间复杂度：O(1)
+         */
 //        int k = 0;
 //        for(int i=0; i<nums.length; i++)
 //            if(nums[i]!=0)
@@ -55,9 +61,11 @@ public class MoveZeroes {
 //        for(int i=k; i<nums.length; i++)
 //            nums[i] = 0;
 
-        // 优化2：swap 0和非0（用游标k，就不用再for循环了）
-        // 时间复杂度：O(n)
-        // 空间复杂度：O(1)
+        /**
+         * 思路4：直接交换0和非0
+         * 时间复杂度：O(n)
+         * 空间复杂度：O(1)
+         */
         int k = 0;
         for(int i=0; i<nums.length; i++)
             if(nums[i]!=0){
---
+++ algorithm/src/com/imooc/algorithm/array/SortColors.java
@@ -0,0 +1,84 @@
+package com.imooc.algorithm.array;
+
+import java.util.Arrays;
+
+public class SortColors {
+
+    /**
+     * 75. Sort Colors
+     * https://leetcode.com/problems/sort-colors/
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     */
+    public void sortColors(int[] nums){
+
+        /**
+         * 思路1：调用Arrays.sort（快排）
+         * 时间复杂度：O(nlogn)
+         * 空间复杂度：O(1)
+         */
+//        Arrays.sort(nums);
+
+
+        /**
+         * 思路2：使用计数排序：count各种颜色总数分别有多少，然后按顺序放回
+         * 时间复杂度：O(n)
+         * 空间复杂度：O(k)
+         */
+//        // count for 0,1,2
+//        int[] count = {0,0,0};
+//        for(int i=0; i<nums.length; i++){
+//            assert(nums[i]>=0 && nums[i]<=2);
+//            count[nums[i]]++;
+//        }
+//        // sort
+//        int index=0;
+//        for(int i=0; i<count[0]; i++)
+//            nums[index++] = 0;
+//        for(int i=0; i<count[1]; i++)
+//            nums[index++] = 1;
+//        for(int i=0; i<count[2]; i++)
+//            nums[index++] = 2;
+
+        /**
+         * 思路3：使用三路快排：只需一次for循环
+         * 时间复杂度：O(n)
+         * 空间复杂度：O(1)
+         */
+        int zero = -1;
+        int two = nums.length;
+        for(int i=0; i<two; ){
+            // 1
+            if(nums[i]==1)
+                i++;
+            // 2
+            else if(nums[i]==2){
+                two--;
+                swap(nums, i, two);
+            }
+            // 0
+            else{
+                assert nums[i]==0;
+                zero++;
+                swap(nums, i, zero);
+                i++;
+            }
+        }
+
+    }
+
+    // swap
+    public void swap(int[] nums, int i, int j){
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+
+    // main
+    public static void main(String[] args) {
+        int[] nums = {2,2,2,1,1,0};
+        (new SortColors()).sortColors(nums);
+        for(int i : nums)
+            System.out.print(i+" ");
+    }
+}
