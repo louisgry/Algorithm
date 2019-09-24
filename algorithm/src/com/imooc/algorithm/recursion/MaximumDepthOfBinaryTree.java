a417919a4e189555eccccfeb2c012af555c88957
louis <244187563@qq.com>
2019-09-25 01:30:43 +0800

    quick commit

--- algorithm/src/com/imooc/algorithm/recursion/MaximumDepthOfBinaryTree.java
+++ algorithm/src/com/imooc/algorithm/recursion/MaximumDepthOfBinaryTree.java
@@ -20,17 +20,17 @@ public class MaximumDepthOfBinaryTree {
      * 空间复杂度：O(h)，h是树深度
      */
     public int maxDepth(TreeNode root) {
+        // 递归终止条件
         if (root==null) {
             return 0;
         }
-        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+        // 递归过程
+        return Math.max(maxDepth(root.left), maxDepth(root.right))+1;
     }
-    /**
-     * main
-     */
+    /** main */
     public static void main(String[] args) {
         MaximumDepthOfBinaryTree maximumDepthOfBinaryTree = new MaximumDepthOfBinaryTree();
-        // 成员内部类：[3,9,20,null,null,15,7]
+        // 成员内部类：Binary Tree [3,9,20,null,null,15,7]
         TreeNode treeNode = maximumDepthOfBinaryTree.new TreeNode(3);
         treeNode.left = maximumDepthOfBinaryTree.new TreeNode(9);
         treeNode.right = maximumDepthOfBinaryTree.new TreeNode(20);
