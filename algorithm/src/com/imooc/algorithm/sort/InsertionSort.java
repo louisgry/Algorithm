7e9e3d39ad3f303234f32ec7f70f949d07241ee7
louis <244187563@qq.com>
2019-09-24 22:48:00 +0800

    quick commit

--- algorithm/src/com/imooc/algorithm/sort/InsertionSort.java
+++ algorithm/src/com/imooc/algorithm/sort/InsertionSort.java
@@ -7,28 +7,35 @@ public class InsertionSort {
      */
     private InsertionSort(){}
 
-    // sort
+    /** sort */
     public static void sort(int[] arr){
         /** 基础版 */
 //        int n = arr.length;
-//        for(int i=0; i<n; i++)
-//            for(int j=i; j>0; j--)
-//                // 如果比前面小则swap
-//                if(arr[j]<arr[j-1])
-//                    swap(arr, j, j-1);
-//                else // 比前面的大就不用插入到前面
+//        for(int i=0; i<n; i++) {
+//            for (int j = i; j > 0; j--) {
+//                // 如果后面(arr[j])比前面(arr[j-1)小则swap
+//                if (arr[j] < arr[j - 1]) {
+//                    swap(arr, j, j - 1);
+//                }
+//                // 否则break
+//                else {
 //                    break;
-
+//                }
+//            }
+//        }
         /** 优化：用赋值替换swap*/
         int n = arr.length;
         for(int i=0; i<n; i++){
             int e = arr[i];
             int j; // j保存e应该插入的位置
-            for(j=i; j>0; j--)
-                if(arr[j-1]>e)
-                    arr[j] = arr[j-1];
-                else
+            for(j=i; j>0; j--) {
+                if (arr[j - 1] > e) {
+                    arr[j] = arr[j - 1];
+                }
+                else {
                     break;
+                }
+            }
             arr[j] = e;
         }
     }
@@ -40,12 +47,13 @@ public class InsertionSort {
         arr[j] = t;
     }
 
-    // main
+    /** main */
     public static void main(String[] args) {
         int[] arr = {9,8,7,6,5,4,3,2,1};
         InsertionSort.sort(arr);
-        for(int i : arr)
-            System.out.print(i+" ");
+        for(int i : arr) {
+            System.out.print(i + " ");
+        }
     }
 
 }
