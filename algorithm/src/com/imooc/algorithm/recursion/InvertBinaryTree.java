175b60f3c27d15ce18e2be255292e9603a5d68b8
louis <244187563@qq.com>
2019-09-28 11:46:14 +0800

    quick commit

--- algorithm/src/com/imooc/algorithm/recursion/InvertBinaryTree.java
+++ algorithm/src/com/imooc/algorithm/recursion/InvertBinaryTree.java
@@ -2,11 +2,7 @@ package com.imooc.algorithm.recursion;
 
 /**
  * @Author: Louis
-<<<<<<< HEAD
- * @Date: Create in 2019/9/25 14:36
-=======
  * @Date: Create in 2019/9/28 10:43
->>>>>>> origin/master
  * @Description:
  */
 public class InvertBinaryTree {
@@ -15,32 +11,21 @@ public class InvertBinaryTree {
         int val;
         TreeNode left;
         TreeNode right;
-<<<<<<< HEAD
-        TreeNode(int x) { val=x; }
-=======
         TreeNode(int x) { val = x; }
->>>>>>> origin/master
     }
     /**
      * 226 Invert Binary Tree
      * https://leetcode.com/problems/invert-binary-tree/
-<<<<<<< HEAD
-     *
-=======
      * 思路：递归
      * 时间复杂度：O(n)
      * 空间复杂度：O(h)
->>>>>>> origin/master
      */
     public TreeNode invertTree(TreeNode root) {
         // 递归终止条件
         if (root==null) {
             return null;
         }
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/master
         // 递归过程
         TreeNode left = invertTree(root.left);
         TreeNode right = invertTree(root.right);
@@ -49,18 +34,11 @@ public class InvertBinaryTree {
         root.right = left;
         return root;
     }
-<<<<<<< HEAD
-    /** main */
-    public static void main(String[] args) {
-        // binary tree [4,2,7,1,3,6,9]
-        InvertBinaryTree invertBinaryTree = new InvertBinaryTree();
-=======
 
     /** main */
     public static void main(String[] args) {
         InvertBinaryTree invertBinaryTree = new InvertBinaryTree();
         // binary tree [4,2,7,1,3,6,9]
->>>>>>> origin/master
         TreeNode treeNode = invertBinaryTree.new TreeNode(4);
         treeNode.left = invertBinaryTree.new TreeNode(2);
         treeNode.right = invertBinaryTree.new TreeNode(7);
@@ -69,16 +47,6 @@ public class InvertBinaryTree {
         treeNode.right.left = invertBinaryTree.new TreeNode(6);
         treeNode.right.right = invertBinaryTree.new TreeNode(9);
         // ==> binary tree [4,7,2,9,6,3,1]
-<<<<<<< HEAD
-        TreeNode resultTreeNode = invertBinaryTree.invertTree(treeNode);
-        System.out.print(resultTreeNode.val + " ");
-        System.out.print(resultTreeNode.left.val + " ");
-        System.out.print(resultTreeNode.right.val + " ");
-        System.out.print(resultTreeNode.left.left.val + " ");
-        System.out.print(resultTreeNode.left.right.val + " ");
-        System.out.print(resultTreeNode.right.left.val + " ");
-        System.out.print(resultTreeNode.right.right.val);
-=======
         TreeNode resultTree = invertBinaryTree.invertTree(treeNode);
         System.out.print(resultTree.val + " ");
         System.out.print(resultTree.left.val + " ");
@@ -87,6 +55,5 @@ public class InvertBinaryTree {
         System.out.print(resultTree.left.right.val + " ");
         System.out.print(resultTree.right.left.val + " ");
         System.out.print(resultTree.right.right.val + " ");
->>>>>>> origin/master
     }
 }
