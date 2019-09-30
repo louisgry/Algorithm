dc04fecc199d76ba2976d3ef84f3a9ca3eebd28b
louis <244187563@qq.com>
2019-10-01 00:08:14 +0800

    quick commit

---
+++ algorithm/src/com/imooc/algorithm/recursion/SumOfLeftLeaves.java
@@ -0,0 +1,50 @@
+package com.imooc.algorithm.recursion;
+
+/**
+ * @Author: Louis
+ * @Date: Create in 2019/10/1 16:25
+ * @Description:
+ */
+public class SumOfLeftLeaves {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    /**
+     * 404 Sum of Left Leaves
+     * https://leetcode.com/problems/sum-of-left-leaves/
+     * 思路：递归
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(h)
+     */
+    public int sumOfLeftLeaves(TreeNode root) {
+        int sum = 0;
+        // 递归终止条件
+        if (root == null) {
+            return 0;
+        }
+        if (root.left!=null && root.left.left==null && root.left.right==null) {
+            sum += root.left.val;
+        }
+        // 递归过程
+        sum += sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+        return sum;
+    }
+    /** main */
+    public static void main(String[] args) {
+        SumOfLeftLeaves sumOfLeftLeavesClass = new SumOfLeftLeaves();
+        // binary tree [3,9,20,null,null,15,7]
+        TreeNode treeNode = sumOfLeftLeavesClass.new TreeNode(3);
+        treeNode.left = sumOfLeftLeavesClass.new TreeNode(9);
+        treeNode.right = sumOfLeftLeavesClass.new TreeNode(20);
+        treeNode.right.left = sumOfLeftLeavesClass.new TreeNode(15);
+        treeNode.right.right = sumOfLeftLeavesClass.new TreeNode(7);
+        // ==> 24
+        System.out.println(sumOfLeftLeavesClass.sumOfLeftLeaves(treeNode));
+    }
+
+}
