ce1e8706258a3fb590c2270c19669e1855bb9423
louis <244187563@qq.com>
2019-11-30 19:11:24 +0800

    quick commit

--- day-day-up/src/ds/BinaryTreeProblem.java
+++ day-day-up/src/ds/BinaryTreeProblem.java
@@ -1,5 +1,5 @@
 package ds;
-
+import java.util.*;
 /**
  * @author Louis
  * @date Create in 2019/11/27 10:01
@@ -11,6 +11,154 @@ public class BinaryTreeProblem {
         TreeNode left;
         TreeNode right;
         TreeNode(int x) { val = x; }
+    }
+    /**
+     * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) {
+            return null;
+        }
+        // 特例1：都在左边
+        if(p.val<root.val && q.val<root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        // 特例2：都在右边
+        if(p.val>root.val && q.val>root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        // 其他情况
+        return root;
+    }
+
+    /**
+     * https://leetcode.com/problems/path-sum-iii/
+     */
+    public int pathSum(TreeNode root, int sum) {
+        if(root == null) {
+            return 0;
+        }
+        // 递归嵌套递归
+        int res = getPathNum(root, sum);
+        res += pathSum(root.left, sum) + pathSum(root.right, sum);
+        return res;
+    }
+    private int getPathNum(TreeNode node, int sum) {
+        if(node == null) {
+            return 0;
+        }
+        int res = 0;
+        if(node.val == sum) {
+            res += 1;
+        }
+        res += getPathNum(node.left, sum-node.val);
+        res += getPathNum(node.right, sum-node.val);
+        return res;
+    }
+
+    /**
+     * https://leetcode.com/problems/sum-root-to-leaf-numbers/
+     */
+    public int sumNumbers(TreeNode root) {
+        return getSum(root, 0);
+    }
+    private int getSum(TreeNode root, int curSum) {
+        if(root == null) {
+            return 0;
+        }
+        curSum = 10*curSum + root.val;
+        if(root.left==null && root.right==null) {
+            return curSum;
+        }
+        return getSum(root.left, curSum) + getSum(root.right, curSum);
+    }
+    /**
+     * https://leetcode.com/problems/path-sum-ii/
+     */
+    public ArrayList<ArrayList<Integer>> pyyathSum(TreeNode root, int sum) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        List<Integer> middle = new ArrayList<Integer>();
+        getPath(root, sum, middle, res);
+        return res;
+    }
+    private void getPath(TreeNode root, int sum, List<Integer> middle, ArrayList<ArrayList<Integer>> res) {
+        if(root == null) {
+            return;
+        }
+        middle.add(root.val);
+        if(root.left==null && root.right==null) {
+            if(root.val == sum) {
+                res.add(new ArrayList<Integer>(middle));
+            }
+        }
+        getPath(root.left, sum-root.val, middle, res);
+        getPath(root.right, sum-root.val, middle, res);
+        middle.remove(middle.size()-1);
+    }
+
+    /**
+     * https://leetcode.com/problems/binary-tree-paths/
+     */
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<String>();
+        // condition
+        if(root == null) {
+            return res;
+        }
+        if(root.left==null && root.right==null) {
+            res.add(Integer.toString(root.val));
+            return res;
+        }
+        // recursion
+        List<String> leftStr = binaryTreePaths(root.left);
+        for(int i=0; i<leftStr.size(); i++) {
+            // res.add(Integer.toString(root.val)+"->"+leftStr.get(i));
+            StringBuilder sb = new StringBuilder(Integer.toString(root.val));
+            sb.append("->");
+            sb.append(leftStr.get(i));
+            res.add(sb.toString());
+        }
+        List<String> rightStr = binaryTreePaths(root.right);
+        for(int i=0; i<rightStr.size(); i++) {
+            // res.add(Integer.toString(root.val)+"->"+rightStr.get(i));
+            StringBuilder sb = new StringBuilder(Integer.toString(root.val));
+            sb.append("->");
+            sb.append(rightStr.get(i));
+            res.add(sb.toString());
+        }
+        return res;
+    }
+
+    /**
+     * https://leetcode.com/problems/sum-of-left-leaves/
+     */
+    public int sumOfLeftLeaves(TreeNode root) {
+        int sum = 0;
+        if(root == null) {
+            return 0;
+        }
+        // 判断左叶子
+        if(root.left!=null && root.left.left==null && root.left.right==null) {
+            sum += root.left.val;
+        }
+        sum += sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+        return sum;
+    }
+
+    /**
+     * https://leetcode.com/problems/path-sum/
+     */
+    public boolean hasPathSum(TreeNode root, int sum) {
+        // condition：root一定得是叶子节点
+        if(root == null) {
+            return false;
+        }
+        if(root.left==null && root.right==null) {
+            return root.val == sum;
+        }
+
+        // condition：关键在于将求和改为相减
+        return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val);
     }
 
     /**
@@ -32,6 +180,15 @@ public class BinaryTreeProblem {
             return 0;
         }
         return Math.max(getDepth(node.left), getDepth(node.right)) + 1;
+    }
+    /**
+     * https://leetcode.com/problems/count-complete-tree-nodes/
+     */
+    public int countNodes(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        return countNodes(root.left) + countNodes(root.right) + 1;
     }
     /**
      * https://leetcode.com/problems/symmetric-tree/
@@ -87,6 +244,26 @@ public class BinaryTreeProblem {
             return false;
         }
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    /**
+     * https://leetcode.com/problems/minimum-depth-of-binary-tree/
+     */
+    public int minDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        if(root.left==null && root.right==null) {
+            return 1;
+        }
+        int min = Integer.MAX_VALUE;
+        if(root.left != null) {
+            min =  Math.min(min, minDepth(root.left) + 1);
+        }
+        if(root.right != null) {
+            min =  Math.min(min, minDepth(root.right) + 1);
+        }
+        return min;
     }
 
     /**
