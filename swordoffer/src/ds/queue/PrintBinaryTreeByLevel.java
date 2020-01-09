b9f6cace46a17d43c043f19296fd6cd56a232a58
louis <244187563@qq.com>
2020-01-09 22:37:49 +0800

    quick commit

--- swordoffer/src/ds/queue/PrintBinaryTreeByLevel.java
+++ swordoffer/src/ds/queue/PrintBinaryTreeByLevel.java
@@ -9,13 +9,13 @@ import java.util.Queue;
  * @date Create in 2020/1/8 21:02
  */
 public class PrintBinaryTreeByLevel {
+
     public class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
         TreeNode(int x) { val = x; }
     }
-
     /**
      * 32.2-把二叉树打印成多行
      * - https://www.nowcoder.com/practice/445c44d982d04483b04a54f298796288
--- swordoffer/src/ds/queue/PrintBinaryTreeZhi.java
+++ swordoffer/src/ds/queue/PrintBinaryTreeZhi.java
@@ -1,14 +1,15 @@
 package ds.queue;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 
 /**
  * @author Louis
- * @date Create in 2020/1/8 20:55
+ * @date Create in 2020/1/9 20:58
  */
-public class PrintBinaryTreeFromHome {
+public class PrintBinaryTreeZhi {
 
     public class TreeNode {
         int val;
@@ -17,15 +18,17 @@ public class PrintBinaryTreeFromHome {
         TreeNode(int x) { val = x; }
     }
     /**
-     * 32.1-从上往下打印二叉树
-     * - https://www.nowcoder.com/practice/7fe2212963db4790b57431d9ed259701
+     * 32.3-按之字形顺序打印二叉树
+     * - https://www.nowcoder.com/practice/91b69814117f4e8097390d107d2efbe0
      */
-    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+    public ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
         // 队列，层序遍历
         Queue<TreeNode> queue = new LinkedList<>();
-        ArrayList<Integer> res = new ArrayList<>();
-        queue.add(root);
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        queue.add(pRoot);
+        boolean rev = false;
         while(!queue.isEmpty()) {
+            ArrayList<Integer> list = new ArrayList<>();
             // 当前队列的size就是该层的节点数
             int count = queue.size();
             while(count-- > 0) {
@@ -33,9 +36,16 @@ public class PrintBinaryTreeFromHome {
                 if(t == null) {
                     continue;
                 }
-                res.add(t.val);
+                list.add(t.val);
                 queue.add(t.left);
                 queue.add(t.right);
+            }
+            if(rev == true) {
+                Collections.reverse(list);
+            }
+            rev = !rev;
+            if(list.size() != 0) {
+                res.add(list);
             }
         }
         return res;
